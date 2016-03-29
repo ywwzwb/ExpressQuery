65bb01c5301df4c1aabb8bfafd8720016e6b1b9a
曾文斌 <zeng.wenbin@datatom.com>
2016-03-29 11:46:14 +0800

    导入文件

---
+++ ExpressQuery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExpressQuery
+//
+//  Created by 曾文斌 on 16/3/28.
+//  Copyright © 2016年 zengwenbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
